40616f27fc3dd83e5fd1210055f616ec3712c7af
Ohood Aljohani <59482214+OhoodAljohani@users.noreply.github.com>
2022-06-08 09:02:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,6 @@ def get_output():
 
 if __name__ =='__main__':
 	#app.debug = True
-	app.run(debug = True)
+	#app.run(debug = True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=True)
